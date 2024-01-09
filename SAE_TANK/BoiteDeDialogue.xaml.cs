f542823bb8920d88f83e0c755d46dc297a67364b
mano Berthet <mano.berthet@etu.univ-smb.fr>
2024-01-09 09:24:13 +0100

    tank bouton

--- SAE_TANK/BoiteDeDialogue.xaml.cs
+++ SAE_TANK/BoiteDeDialogue.xaml.cs
@@ -25,7 +25,29 @@ namespace SAE_TANK
             ImageBrush background = new ImageBrush();
             background.ImageSource = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "Menu.jpg"));
             backgroundMenu.Fill = background;
-            Tank_Bleu_1.Background = background;
+            ImageBrush tankB1 = new ImageBrush();
+            ImageBrush tankB2 = new ImageBrush();
+            ImageBrush tankB3 = new ImageBrush();
+            ImageBrush tankR1 = new ImageBrush();
+            ImageBrush tankR2 = new ImageBrush();
+            ImageBrush tankR3 = new ImageBrush();
+
+            tankB1.ImageSource = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "image_Tanks/Tank_bleu_1_S.png"));
+            tankB2.ImageSource = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "image_Tanks/Tank_bleu_2_S.png"));
+            tankB3.ImageSource = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "image_Tanks/Tank_bleu_3_S.png"));
+            tankR1.ImageSource = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "image_Tanks/Tank_rouge_1_S.png"));
+            tankR2.ImageSource = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "image_Tanks/Tank_rouge_2_S.png"));
+            tankR3.ImageSource = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "image_Tanks/Tank_rouge_3_S.png"));
+
+            Tank_bleu_1.Background = tankB1;
+            Tank_bleu_2.Background = tankB2;
+            Tank_bleu_3.Background = tankB3;
+
+            Tank_rouge_1.Background = tankR1;
+            Tank_rouge_2.Background = tankR2;
+            Tank_rouge_3.Background = tankR3;
+
+
         }
     }
 }
